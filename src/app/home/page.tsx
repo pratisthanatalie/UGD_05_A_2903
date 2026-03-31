732d1e8f75f89e202cddb1280e7f720c5d066bcc
pratisthanatalie <pratisthanatalie@gmail.com>
2026-03-31 21:33:06 +0700

    alamak

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,27 @@
 'use client';
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Game1 from "../../components/Game1";
 
 export default function Home() {
   const router = useRouter();
+  const [checking, setChecking] = useState(true);
 
   useEffect(() => {
     const isLoggedIn = localStorage.getItem('isLoggedIn');
 
     if (!isLoggedIn) {
-      router.push('/auth/not-authorized');
+      router.replace('/auth/not-authorized');
+    } else {
+      setChecking(false);
     }
   }, []);
 
+  if (checking) return null; 
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
       <h1 className="text-4xl font-bold mb-4">Selamat Datang!</h1>
       <Game1 />
     </div>
