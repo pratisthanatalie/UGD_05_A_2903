68281c0a3c2ad481d26747cc208970e9c86e2c22
pratisthanatalie <pratisthanatalie@gmail.com>
2026-04-01 09:46:45 +0700

    kelarekk

--- src/app/auth/not-authorized/page.tsx
+++ src/app/auth/not-authorized/page.tsx
@@ -10,35 +10,35 @@ export default function NotAuthorized() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br">
       <div className="bg-white/80 backdrop-blur-md p-5 rounded-2xl shadow-2xl w-[350px] text-center">
 
-        {/* IMAGE */}
-        <img
-         src="/gambarugd5.jpg"
-         alt="not authorized"
-         width={300}
-         height={200}
-         className="rounded-lg object-cover"
-        />
-      </div>
+  {/* IMAGE */}
+  <Image
+  src="/gambarugd5.jpg"
+  alt="not authorized"
+  width={300}
+  height={200}
+  className="rounded-lg object-cover mx-auto"
+/>
 
-        {/* TITLE */}
-        <h2 className="text-lg font-bold text-gray-800 flex items-center justify-center gap-2 mb-1">
-          <span className="text-red-500 text-xl">✖</span>
-          Anda belum login
-        </h2>
+  {/* TITLE */}
+  <h2 className="text-lg font-bold text-gray-800 flex items-center justify-center gap-2 mb-1 mt-3">
+    <span className="text-red-500 text-xl">✖</span>
+    Anda belum login
+  </h2>
 
-        {/* SUBTEXT */}
-        <p className="text-sm text-gray-500 mb-4">
-          Silakan login terlebih dahulu
-        </p>
+  {/* SUBTEXT */}
+  <p className="text-sm text-gray-500 mb-4">
+    Silakan login terlebih dahulu
+  </p>
 
-        {/* BUTTON */}
-        <button
-          onClick={() => router.push('/auth/login?unauthorized=true')}
-          className="bg-blue-600 text-white px-5 py-2 rounded-lg flex items-center justify-center gap-2 mx-auto hover:bg-blue-700 transition"
-        >
-          ← Kembali
-        </button>
+  {/* BUTTON */}
+  <button
+    onClick={() => router.push('/auth/login?unauthorized=true')}
+    className="bg-blue-600 text-white px-5 py-2 rounded-lg flex items-center justify-center gap-2 mx-auto hover:bg-blue-700 transition"
+  >
+    ← Kembali
+  </button>
 
-      </div>
+</div>
+    </div>
   );
 }
