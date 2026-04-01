deb83647053d8dd063fec8a4d1bf3e92ec7876ab
pratisthanatalie <pratisthanatalie@gmail.com>
2026-04-01 20:43:37 +0700

    kelarekkkkkkkkk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+import Game1 from '../components/Game1';
 
 export default function Home() {
-  redirect('/auth/login');
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+    if (!isLoggedIn) {
+      router.push('/auth/not-authorized');
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  if (!isAuthenticated) {
+    return null; 
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Game1 />
+    </div>
+  );
 }
